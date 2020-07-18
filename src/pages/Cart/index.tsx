8f4c4d435d6e80de00d5abbe6e4b59ea9afb1c00
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-18 18:14:02 -0300

    add products to cart and show total

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,21 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
+      return accumulator + productsSubtotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+      return accumulator + productsQuantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
